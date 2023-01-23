c2718a6ffa5807f918030fe664510257d91ccacc
Max-Dor <la2web@hotmail.com>
2023-01-23 22:52:42 +0300

    Написал функцию по созданию разметки - нагородил херни

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ const apartsList =[{
 {
     category: 'studio',
     id: 4,
-    info: 'Студия на  третьем этаже.',
+    info: 'Студия на третьем этаже.',
     image: ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg']
 },
 {
@@ -80,6 +80,103 @@ const apartsList =[{
     image: ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg']
 }];
 
+
+apartsList.forEach(apart=>{
+    if(apart.category == 'studio'){
+        console.log(apart.info)
+    }
+})
+
+const body = document.body;
+
+// function createCard(title){
+//     const overflow = document.createElement('div');
+//     const span = document.createElement('span');
+//     const cardTitle = document.createElement('h2');
+//         overflow.classList.add('card__overflow');
+//         cardTitle.textContent = title;
+//         span.classList.add('close__card');
+//         body.appendChild(overflow);
+//         overflow.appendChild(span);
+//         overflow.appendChild(cardTitle);
+// }
+// createCard('задница');
+
+// function apartCard(info){
+//     const roomCard = document.createElement('div');
+//     const  roomInfo = document.createElement('h3');
+//         roomInfo.textContent = info
+//         roomCard.classList.add('room__card');
+//         roomInfo.classList.add('room__info');
+//         roomCard.appendChild(roomInfo);
+// }
+// apartCard(apartsList.info);
+
+// function roomImageCreator(image){
+//     const ulList = document.createElement('ul');
+//     const imgList = document.createElement('li');
+//     const roomImg = document.createElement('img');
+//         roomImg.src = image;
+//         ulList.classList.add('ul__list');
+//         imgList.classList.add('img__list');
+//         roomImg.classList.add('room__img');
+
+// }
+// roomImageCreator(apartsList.image);
+
+
+
+function createCard(title,info){
+    const overflow = document.createElement('section');
+    const mainCard = document.createElement('div');
+    const span = document.createElement('span');
+    const cardTitle = document.createElement('h2');
+        overflow.classList.add('overflow');
+        mainCard.classList.add('main__card');
+        cardTitle.textContent = title;
+        span.classList.add('close__card');
+        body.appendChild(overflow);
+        overflow.appendChild(mainCard)
+        mainCard.appendChild(span);
+        mainCard.appendChild(cardTitle);
+        function apartCard(info){
+            const roomCard = document.createElement('div');
+            const  roomInfo = document.createElement('h3');
+                roomInfo.textContent = info;
+                roomCard.classList.add('room__card');
+                roomInfo.classList.add('room__info');
+                roomCard.appendChild(roomInfo);
+                mainCard.appendChild(roomCard);
+                function roomImageCreator(image){
+                    const ulList = document.createElement('ul');
+                    const imgList = document.createElement('li');
+                    const roomImg = document.createElement('img');
+                        roomImg.src = image;
+                        ulList.classList.add('ul__list');
+                        imgList.classList.add('img__list');
+                        roomImg.classList.add('room__img');
+                        roomCard.appendChild(ulList);
+                        ulList.appendChild(imgList);
+                        imgList.appendChild(roomImg);
+                
+                }
+                roomImageCreator(apartsList.image);
+        }
+        apartCard(apartsList.info);
+}
+createCard('Название выбраного блока',apartsList.info);
+
+
+
+
+
+
+
+
+
+
+
+
 // const showCards = document.querySelectorAll('.show__card');
 //     showCards.forEach(function(e){
 //         e.addEventListener('click',()=>{
@@ -95,29 +192,29 @@ const apartsList =[{
 //     })
 // })
 
-const overflow = document.createElement('div');
-    overflow.classList.add('card__overflow');
+// const overflow = document.createElement('div');
+//     overflow.classList.add('card__overflow');
 
-function generateCards(title,info,image){
-    return`
-    <div class="cardholder"> 
-        <span class="close__card"></span>
-        <h2 class="cards__title">${title}</h2>
-            <div class="card__list">
-                <h3 class="card__info">${info}</h3>
-                <ul class="ul__list">
-                    <li class="img__list">
-                        <img class="card__img" src="${image}">
-                </ul>
-            </div>
-        </div>
-    `;
-}
-generateCards()
+// function generateCards(title,info,image){
+//     return`
+//     <div class="cardholder"> 
+//         <span class="close__card"></span>
+//         <h2 class="cards__title">${title}</h2>
+//             <div class="card__list">
+//                 <h3 class="card__info">${info}</h3>
+//                 <ul class="ul__list">
+//                     <li class="img__list">
+//                         <img class="card__img" src="${image}">
+//                 </ul>
+//             </div>
+//         </div>
+//     `;
+// }
+// generateCards()
 
-const apartsHTML = apartsList.map(apart =>{
-    return generateCards(apart.title,apart.info,apart.image)
-}).join('');
+// const apartsHTML = apartsList.map(apart =>{
+//     return generateCards(apart.title,apart.info,apart.image)
+// }).join('');
 
-console.log(apartsHTML)
+// console.log(apartsHTML)
 
