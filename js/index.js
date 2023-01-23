41d1f1d9c5a8d7d2469b603853bcad19e0a50f4c
Max-Dor <la2web@hotmail.com>
2023-01-23 23:25:10 +0300

    Написал ерунду в js файле

--- js/index.js
+++ js/index.js
@@ -30,104 +30,191 @@ const navigation = document.querySelector('.navigation');
     });
 
 
-
-
-const apartments = [];
-
-const firstStudioImg = ['/image/photos/studios/lev3_stu1_01.jpg','/image/photos/studios/lev3_stu1_02.jpg','/image/photos/studios/lev3_stu1_03.jpg','/image/photos/studios/lev3_stu1_04.jpg','/image/photos/studios/lev3_stu1_05.jpg','/image/photos/studios/lev3_stu1_06.jpg'],
-    secondStudiosImg = ['/image/photos/studios/lev6_stu2_01.jpg','/image/photos/studios/lev6_stu2_02.jpg','/image/photos/studios/lev6_stu2_03.jpg'],
-    studioTerraceImg = ['/image/photos/terrace/lev5_ter_01.jpg','/image/photos/terrace/lev5_ter_02.jpg','/image/photos/terrace/lev5_ter_03.jpg','/image/photos/terrace/lev5_ter_04.jpg','/image/photos/terrace/lev5_ter_05.jpg'],
-    studio25Img = ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg'],
-    twoRoomsImg = ['/image/photos/room2/lev3_room2_01.jpg','/image/photos/room2/lev3_room2_02.jpg','/image/photos/room2/lev3_room2_03.jpg','/image/photos/room2/lev3_room2_04.jpg','/image/photos/room2/lev3_room2_05.jpg'],
-    firstThreeRoomImg = ['/image/photos/room3/lev5_room3_01.jpg','/image/photos/room3/lev5_room3_02.jpg','/image/photos/room3/lev5_room3_03.jpg','/image/photos/room3/lev5_room3_04.jpg','/image/photos/room3/lev5_room3_05.jpg','/image/photos/room3/lev5_room3_06.jpg'],
-    secondThreeRoomImg = ['/image/photos/room3/lev6_room3_01.jpg','/image/photos/room3/lev6_room3_02.jpg','/image/photos/room3/lev6_room3_03.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_05.jpg','/image/photos/room3/lev6_room3_06.jpg','/image/photos/room3/lev6_room3_07.jpg','/image/photos/room3/lev6_room3_08.jpg','/image/photos/room3/lev6_room3_09.jpg','/image/photos/room3/lev6_room3_10.jpg'],
-    thirdThreeRoomImg = ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg'];
-
-
-const firstStudio = {
+const apartsList =[{
+    category: 'studio',
     id: 1,
-    title: 'Студия на третьем этаже.',
-    image: firstStudioImg
+    info: 'Студия на третьем этаже.',
+    image: ['/image/photos/studios/lev3_stu1_01.jpg','/image/photos/studios/lev3_stu1_02.jpg','/image/photos/studios/lev3_stu1_03.jpg','/image/photos/studios/lev3_stu1_04.jpg','/image/photos/studios/lev3_stu1_05.jpg','/image/photos/studios/lev3_stu1_06.jpg']
     
-};
-const secondStudio = {
+},
+{
+    category: 'studio',
     id: 2,
-    title: 'Студия на шестом этаже.',
-    image: secondStudiosImg
-};
-const studioTerrace = {
+    info: 'Студия на шестом этаже.',
+    image: ['/image/photos/studios/lev6_stu2_01.jpg','/image/photos/studios/lev6_stu2_02.jpg','/image/photos/studios/lev6_stu2_03.jpg']
+},
+{
+    category: 'terrace',
     id: 3,
-    title: 'Cтудия на пятом этажею',
-    image: studioTerraceImg
-};
-const studio25 = {
+    info: 'Cтудия на пятом этажею',
+    image: ['/image/photos/terrace/lev5_ter_01.jpg','/image/photos/terrace/lev5_ter_02.jpg','/image/photos/terrace/lev5_ter_03.jpg','/image/photos/terrace/lev5_ter_04.jpg','/image/photos/terrace/lev5_ter_05.jpg']
+},
+{
+    category: 'studio',
     id: 4,
-    title: 'Студия на  третьем этаже.',
-    image: studio25Img
-};
-const twoRooms = {
+    info: 'Студия на третьем этаже.',
+    image: ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg']
+},
+{
+    category: 'tworooms',
     id: 5,
-    title: 'Двухкомнатные апартаменты на  третьем этаже. '
-};
-const firstThreeRoom = {
+    info: 'Двухкомнатные апартаменты на  третьем этаже. ',
+    image: ['/image/photos/room2/lev3_room2_01.jpg','/image/photos/room2/lev3_room2_02.jpg','/image/photos/room2/lev3_room2_03.jpg','/image/photos/room2/lev3_room2_04.jpg','/image/photos/room2/lev3_room2_05.jpg']
+},
+{
+    category: 'threerooms',
     id: 6,
-    title: 'Трёхкомнатные апартаменты на пятом этаже.',
-    image: firstThreeRoomImg
-};
-const secondThreeRoom = {
+    info: 'Трёхкомнатные апартаменты на пятом этаже.',
+    image: ['/image/photos/room3/lev5_room3_01.jpg','/image/photos/room3/lev5_room3_02.jpg','/image/photos/room3/lev5_room3_03.jpg','/image/photos/room3/lev5_room3_04.jpg','/image/photos/room3/lev5_room3_05.jpg','/image/photos/room3/lev5_room3_06.jpg']
+},
+{
+    category: 'threerooms',
     id: 7,
-    title: 'Трёхкомнатные апартаменты на шестом этаже.',
-    image: secondThreeRoomImg
-};
-const thirdThreeRoom = {
+    info: 'Трёхкомнатные апартаменты на шестом этаже.',
+    image: ['/image/photos/room3/lev6_room3_01.jpg','/image/photos/room3/lev6_room3_02.jpg','/image/photos/room3/lev6_room3_03.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_05.jpg','/image/photos/room3/lev6_room3_06.jpg','/image/photos/room3/lev6_room3_07.jpg','/image/photos/room3/lev6_room3_08.jpg','/image/photos/room3/lev6_room3_09.jpg','/image/photos/room3/lev6_room3_10.jpg']
+},
+{
+    category: 'threerooms',
     id: 8,
-    title: 'Трёхкомнатные апартаменты на шестом этаже.',
-    image: thirdThreeRoomImg 
-};
+    info: 'Трёхкомнатные апартаменты на втором этаже.',
+    image: ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg']
+}];
 
 
-
-const body = document.body;
-const cardOverflow = document.createElement('div');
-    cardOverflow.classList.add('card__overflow');
-const div = document.createElement('div');
-    div.classList.add('cardholder');
-    body.appendChild(div);
-const cardsTitle = document.createElement('h2');
-const cardInfo = document.createElement('h3');
-const ulList = document.createElement('ul');
-   // cardsTitle.textContent = id.title;
-    cardsTitle.classList.add('cards__title');
-    cardsTitle.textContent = "Название  блока с квартирами";
-    cardInfo.classList.add('card__info');
-    cardInfo.textContent = "Информация о квартире";
-    ulList.classList.add('card__list');
-    div.appendChild(cardsTitle);
-    div.appendChild(cardInfo);
-    div.appendChild(ulList);
-
-const listGenerator = `
-    <li class="card__item">
-        <img src="#">
-    </li>
-`;
-
-ulList.insertAdjacentHTML('beforeEnd' ,listGenerator);
-
-const showCards = document.querySelectorAll('.show__card');
-    showCards.forEach(function(e){
-        e.addEventListener('click',()=>{
-            if(e.dataset.target == 'studio'){
-                console.log('Прожато  studio')
-            }else if(e.dataset.target == 'tworooms'){
-                console.log('Прожато tworooms')
-            }else if(e.dataset.target == 'threeroom'){
-                console.log('Прожато threeroom')
-            }else if(e.dataset.target == 'terrace'){
-                console.log('Прожато terrace')
-            }
-    })
+apartsList.forEach(apart=>{
+    if(apart.category == 'studio'){
+        console.log(apart.info)
+    }
 })
 
+const body = document.body;
 
+// function createCard(title){
+//     const overflow = document.createElement('div');
+//     const span = document.createElement('span');
+//     const cardTitle = document.createElement('h2');
+//         overflow.classList.add('card__overflow');
+//         cardTitle.textContent = title;
+//         span.classList.add('close__card');
+//         body.appendChild(overflow);
+//         overflow.appendChild(span);
+//         overflow.appendChild(cardTitle);
+// }
+// createCard('задница');
+
+// function apartCard(info){
+//     const roomCard = document.createElement('div');
+//     const  roomInfo = document.createElement('h3');
+//         roomInfo.textContent = info
+//         roomCard.classList.add('room__card');
+//         roomInfo.classList.add('room__info');
+//         roomCard.appendChild(roomInfo);
+// }
+// apartCard(apartsList.info);
+
+// function roomImageCreator(image){
+//     const ulList = document.createElement('ul');
+//     const imgList = document.createElement('li');
+//     const roomImg = document.createElement('img');
+//         roomImg.src = image;
+//         ulList.classList.add('ul__list');
+//         imgList.classList.add('img__list');
+//         roomImg.classList.add('room__img');
+
+// }
+// roomImageCreator(apartsList.image);
+
+
+
+function createCard(title,info){
+    const overflow = document.createElement('section');
+    const mainCard = document.createElement('div');
+    const span = document.createElement('span');
+    const cardTitle = document.createElement('h2');
+        overflow.classList.add('overflow');
+        mainCard.classList.add('main__card');
+        cardTitle.textContent = title;
+        span.classList.add('close__card');
+        body.appendChild(overflow);
+        overflow.appendChild(mainCard)
+        mainCard.appendChild(span);
+        mainCard.appendChild(cardTitle);
+        function apartCard(info){
+            const roomCard = document.createElement('div');
+            const  roomInfo = document.createElement('h3');
+                roomInfo.textContent = info;
+                roomCard.classList.add('room__card');
+                roomInfo.classList.add('room__info');
+                roomCard.appendChild(roomInfo);
+                mainCard.appendChild(roomCard);
+                function roomImageCreator(image){
+                    const ulList = document.createElement('ul');
+                    const imgList = document.createElement('li');
+                    const roomImg = document.createElement('img');
+                        roomImg.src = image;
+                        ulList.classList.add('ul__list');
+                        imgList.classList.add('img__list');
+                        roomImg.classList.add('room__img');
+                        roomCard.appendChild(ulList);
+                        ulList.appendChild(imgList);
+                        imgList.appendChild(roomImg);
+                
+                }
+                roomImageCreator(apartsList.image);
+        }
+        apartCard(apartsList.info);
+}
+createCard('Название выбраного блока',apartsList.info);
+
+
+
+
+
+
+
+
+
+
+
+
+// const showCards = document.querySelectorAll('.show__card');
+//     showCards.forEach(function(e){
+//         e.addEventListener('click',()=>{
+//             if(e.dataset.target == 'studio'){
+//                 title = 'Студии...'
+//             }else if(e.dataset.target == 'tworooms'){
+//                 createHtml('2-х комнатные апартаменты...','Подробный интерьер квартиры ...')
+//             }else if(e.dataset.target == 'threeroom'){
+//                 createHtml('3-х комнатные апартаменты...','Внутренний интерьер квартир...')
+//             }else if(e.dataset.target == 'terrace'){
+//                 createHtml('Апартаменты с террасой...', 'Интерьер с великолепным видом на море...')
+//             }
+//     })
+// })
+
+// const overflow = document.createElement('div');
+//     overflow.classList.add('card__overflow');
+
+// function generateCards(title,info,image){
+//     return`
+//     <div class="cardholder"> 
+//         <span class="close__card"></span>
+//         <h2 class="cards__title">${title}</h2>
+//             <div class="card__list">
+//                 <h3 class="card__info">${info}</h3>
+//                 <ul class="ul__list">
+//                     <li class="img__list">
+//                         <img class="card__img" src="${image}">
+//                 </ul>
+//             </div>
+//         </div>
+//     `;
+// }
+// generateCards()
+
+// const apartsHTML = apartsList.map(apart =>{
+//     return generateCards(apart.title,apart.info,apart.image)
+// }).join('');
+
+// console.log(apartsHTML)
 
