55b89f9a09552583f919af728e534553577efb8f
Max-Dor <la2web@hotmail.com>
2023-01-28 22:08:11 +0300

    Работа над ошибками

--- js/index.js
+++ js/index.js
@@ -35,55 +35,55 @@ const apartments = Array.from(document.querySelectorAll('.show__card'));
 
 const data = [{
     id: 11,
-    info: 'Студия на третьем этаже.',
+    title: 'Студия на третьем этаже.',
     image: ['/image/photos/studios/lev3_stu1_01.jpg','/image/photos/studios/lev3_stu1_02.jpg','/image/photos/studios/lev3_stu1_03.jpg','/image/photos/studios/lev3_stu1_04.jpg','/image/photos/studios/lev3_stu1_05.jpg','/image/photos/studios/lev3_stu1_06.jpg']
     
 },
 {
     id: 22,
-    info: 'Студия на шестом этаже.',
+    title: 'Студия на шестом этаже.',
     image: ['/image/photos/studios/lev6_stu2_01.jpg','/image/photos/studios/lev6_stu2_02.jpg','/image/photos/studios/lev6_stu2_03.jpg']
 },
 {
     id: 33,
-    info: 'Cтудия на пятом этажею',
+    title: 'Cтудия на пятом этажею',
     image: ['/image/photos/terrace/lev5_ter_01.jpg','/image/photos/terrace/lev5_ter_02.jpg','/image/photos/terrace/lev5_ter_03.jpg','/image/photos/terrace/lev5_ter_04.jpg','/image/photos/terrace/lev5_ter_05.jpg']
 },
 {
     id: 44,
-    info: 'Студия на третьем этаже.',
+    title: 'Студия на третьем этаже.',
     image: ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg']
 },
 {
     id: 55,
-    info: 'Двухкомнатные апартаменты на  третьем этаже. ',
+    title: 'Двухкомнатные апартаменты на  третьем этаже. ',
     image: ['/image/photos/room2/lev3_room2_01.jpg','/image/photos/room2/lev3_room2_02.jpg','/image/photos/room2/lev3_room2_03.jpg','/image/photos/room2/lev3_room2_04.jpg','/image/photos/room2/lev3_room2_05.jpg']
 },
 {
     id: 66,
-    info: 'Трёхкомнатные апартаменты на пятом этаже.',
+    title: 'Трёхкомнатные апартаменты на пятом этаже.',
     image: ['/image/photos/room3/lev5_room3_01.jpg','/image/photos/room3/lev5_room3_02.jpg','/image/photos/room3/lev5_room3_03.jpg','/image/photos/room3/lev5_room3_04.jpg','/image/photos/room3/lev5_room3_05.jpg','/image/photos/room3/lev5_room3_06.jpg']
 },
 {
     id: 77,
-    info: 'Трёхкомнатные апартаменты на шестом этаже.',
+    title: 'Трёхкомнатные апартаменты на шестом этаже.',
     image: ['/image/photos/room3/lev6_room3_01.jpg','/image/photos/room3/lev6_room3_02.jpg','/image/photos/room3/lev6_room3_03.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_05.jpg','/image/photos/room3/lev6_room3_06.jpg','/image/photos/room3/lev6_room3_07.jpg','/image/photos/room3/lev6_room3_08.jpg','/image/photos/room3/lev6_room3_09.jpg','/image/photos/room3/lev6_room3_10.jpg']
 },
 {
     id: 88,
-    info: 'Трёхкомнатные апартаменты на втором этаже.',
+    title: 'Трёхкомнатные апартаменты на втором этаже.',
     image: ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg']
 }];
 
 
 
-const getMarkup = ({info})=>{
+const getMarkup = ({title})=>{
     return `
         <div class = "card__overflow">
             <div class ="card__main">
             <button class ="card__close"><span class ="card__line"></span></button>
                 <div class ="apart">
-                    <h2 class ="card__info">${info}</h2>
+                    <h2 class ="card__info">${title}</h2>
                     
                 </div>
             </div>
