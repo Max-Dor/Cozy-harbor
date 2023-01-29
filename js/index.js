e2acdf188448e6368535f8925c2469076a8f4768
Max-Dor <la2web@hotmail.com>
2023-01-29 19:51:42 +0300

    Поковырялся  в js

--- js/index.js
+++ js/index.js
@@ -31,45 +31,56 @@ const navigation = document.querySelector('.navigation');
 
 
 const mainDiv = document.querySelector('.main__div');
+const cardMain = document.querySelector('.card__main');
 const apartments = Array.from(document.querySelectorAll('.show__card'));
 
+
 const data = [{
+    category: 'studio',
     id: 11,
     title: 'Студия на третьем этаже.',
     image: ['/image/photos/studios/lev3_stu1_01.jpg','/image/photos/studios/lev3_stu1_02.jpg','/image/photos/studios/lev3_stu1_03.jpg','/image/photos/studios/lev3_stu1_04.jpg','/image/photos/studios/lev3_stu1_05.jpg','/image/photos/studios/lev3_stu1_06.jpg']
     
 },
 {
+    category: 'studio',
     id: 22,
     title: 'Студия на шестом этаже.',
     image: ['/image/photos/studios/lev6_stu2_01.jpg','/image/photos/studios/lev6_stu2_02.jpg','/image/photos/studios/lev6_stu2_03.jpg']
 },
 {
+    category: 'terrace',
     id: 33,
-    title: 'Cтудия на пятом этажею',
+    title: 'Cтудияс террасой на пятом этажею',
     image: ['/image/photos/terrace/lev5_ter_01.jpg','/image/photos/terrace/lev5_ter_02.jpg','/image/photos/terrace/lev5_ter_03.jpg','/image/photos/terrace/lev5_ter_04.jpg','/image/photos/terrace/lev5_ter_05.jpg']
 },
 {
+    category: 'studio',
     id: 44,
     title: 'Студия на третьем этаже.',
     image: ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg']
 },
 {
+    
+    category: 'tworooms',
     id: 55,
     title: 'Двухкомнатные апартаменты на  третьем этаже. ',
     image: ['/image/photos/room2/lev3_room2_01.jpg','/image/photos/room2/lev3_room2_02.jpg','/image/photos/room2/lev3_room2_03.jpg','/image/photos/room2/lev3_room2_04.jpg','/image/photos/room2/lev3_room2_05.jpg']
 },
 {
+    category: 'threerooms',
     id: 66,
     title: 'Трёхкомнатные апартаменты на пятом этаже.',
     image: ['/image/photos/room3/lev5_room3_01.jpg','/image/photos/room3/lev5_room3_02.jpg','/image/photos/room3/lev5_room3_03.jpg','/image/photos/room3/lev5_room3_04.jpg','/image/photos/room3/lev5_room3_05.jpg','/image/photos/room3/lev5_room3_06.jpg']
 },
 {
+    category: 'threerooms',
     id: 77,
     title: 'Трёхкомнатные апартаменты на шестом этаже.',
     image: ['/image/photos/room3/lev6_room3_01.jpg','/image/photos/room3/lev6_room3_02.jpg','/image/photos/room3/lev6_room3_03.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_05.jpg','/image/photos/room3/lev6_room3_06.jpg','/image/photos/room3/lev6_room3_07.jpg','/image/photos/room3/lev6_room3_08.jpg','/image/photos/room3/lev6_room3_09.jpg','/image/photos/room3/lev6_room3_10.jpg']
 },
 {
+    category: 'threerooms',
     id: 88,
     title: 'Трёхкомнатные апартаменты на втором этаже.',
     image: ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg']
@@ -77,39 +88,36 @@ const data = [{
 
 
 
-const getMarkup = ({title})=>{
-    return `
-        <div class = "card__overflow">
-            <div class ="card__main">
-            <button class ="card__close"><span class ="card__line"></span></button>
+const getMarkup = (apartmentData)=>{
+    return apartmentData.map( data =>{
+            return `
                 <div class ="apart">
-                    <h2 class ="card__info">${title}</h2>
-                    
-                </div>
-            </div>
-        </div>
+                    <h2 class ="card__info">${data.title}</h2>
+                    <img class = "list__img" src = ${data.image.map(data =>{
+                        return`
+                        <img class = "list__img-item" src = ${data}>
+                        `
+                    }).join('')}>
     `;
+    }).join('');
 }
-const getUlMarkup = function ({image}){
+const getUlMarkup = function ( ){
     return `
-    <ul class ="ul__cards">
         <li class ="list__card">
-            <img class ="list__img src = "${image}">
+            <img class ="list__img src = "${data.image}">
         </li>
-    </ul>
     `;
 }
 
 function getData(e){
     const{dataset} = e.target;
-    const {aptid} = dataset;
-    const id = Number(aptid);
+    const {target} = dataset;
 
-    const apartmentData = data.find(d => d.id === id);
-
+    const apartmentData = data.filter(d => d.category === target);
+    getMarkup(apartmentData);
     const html = getMarkup(apartmentData);
 
-    console.log(e)
-    mainDiv.innerHTML = html;
+    cardMain.innerHTML = html;
+
 }
 apartments.forEach(key => key.addEventListener('click', getData))
