1de9d72fd9dceee211034bb566386061da735d3b
Max-Dor <la2web@hotmail.com>
2023-02-01 11:27:02 +0300

    Не скейлятся фотки в карточке

--- js/index.js
+++ js/index.js
@@ -36,51 +36,51 @@ const apartments = Array.from(document.querySelectorAll('.show__card'));
 const data = [{
     category: 'studio',
     id: 11,
-    title: 'Студия на третьем этаже.',
+    title: 'Студия на третьем этаже...',
     image: ['/image/photos/studios/lev3_stu1_01.jpg','/image/photos/studios/lev3_stu1_02.jpg','/image/photos/studios/lev3_stu1_03.jpg','/image/photos/studios/lev3_stu1_04.jpg','/image/photos/studios/lev3_stu1_05.jpg','/image/photos/studios/lev3_stu1_06.jpg']
     
 },
 {
     category: 'studio',
     id: 22,
-    title: 'Студия на шестом этаже.',
+    title: 'Студия на шестом этаже...',
     image: ['/image/photos/studios/lev6_stu2_01.jpg','/image/photos/studios/lev6_stu2_02.jpg','/image/photos/studios/lev6_stu2_03.jpg']
 },
 {
     category: 'terrace',
     id: 33,
-    title: 'Cтудияс террасой на пятом этажею',
+    title: 'Cтудияс террасой на пятом этаже...',
     image: ['/image/photos/terrace/lev5_ter_01.jpg','/image/photos/terrace/lev5_ter_02.jpg','/image/photos/terrace/lev5_ter_03.jpg','/image/photos/terrace/lev5_ter_04.jpg','/image/photos/terrace/lev5_ter_05.jpg']
 },
 {
     category: 'studio',
     id: 44,
-    title: 'Студия на третьем этаже.',
+    title: 'Студия на третьем этаже...',
     image: ['/image/photos/studios/lev3_stu25m_01.jpg','/image/photos/studios/lev3_stu25m_02.jpg','/image/photos/studios/lev3_stu25m_03.jpg','/image/photos/studios/lev3_stu25m_04.jpg','/image/photos/studios/lev3_stu25m_05.jpg','/image/photos/studios/lev3_stu25m_06.jpg','/image/photos/studios/lev3_stu25m_07.jpg','/image/photos/studios/lev3_stu25m_08.jpg']
 },
 {
     
     category: 'tworooms',
     id: 55,
-    title: 'Двухкомнатные апартаменты на  третьем этаже. ',
+    title: 'Двухкомнатные апартаменты на  третьем этаже... ',
     image: ['/image/photos/room2/lev3_room2_01.jpg','/image/photos/room2/lev3_room2_02.jpg','/image/photos/room2/lev3_room2_03.jpg','/image/photos/room2/lev3_room2_04.jpg','/image/photos/room2/lev3_room2_05.jpg']
 },
 {
     category: 'threerooms',
     id: 66,
-    title: 'Трёхкомнатные апартаменты на пятом этаже.',
+    title: 'Трёхкомнатные апартаменты на пятом этаже...',
     image: ['/image/photos/room3/lev5_room3_01.jpg','/image/photos/room3/lev5_room3_02.jpg','/image/photos/room3/lev5_room3_03.jpg','/image/photos/room3/lev5_room3_04.jpg','/image/photos/room3/lev5_room3_05.jpg','/image/photos/room3/lev5_room3_06.jpg']
 },
 {
     category: 'threerooms',
     id: 77,
-    title: 'Трёхкомнатные апартаменты на шестом этаже.',
+    title: 'Трёхкомнатные апартаменты на шестом этаже...',
     image: ['/image/photos/room3/lev6_room3_01.jpg','/image/photos/room3/lev6_room3_02.jpg','/image/photos/room3/lev6_room3_03.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_04.jpg','/image/photos/room3/lev6_room3_05.jpg','/image/photos/room3/lev6_room3_06.jpg','/image/photos/room3/lev6_room3_07.jpg','/image/photos/room3/lev6_room3_08.jpg','/image/photos/room3/lev6_room3_09.jpg','/image/photos/room3/lev6_room3_10.jpg']
 },
 {
     category: 'threerooms',
     id: 88,
-    title: 'Трёхкомнатные апартаменты на втором этаже.',
+    title: 'Трёхкомнатные апартаменты на втором этаже...',
     image: ['/image/photos/room3/lev2_room3_07.jpg','/image/photos/room3/lev2_room3_08.jpg','/image/photos/room3/lev2_room3_09.jpg']
 }];
 
@@ -116,6 +116,7 @@ function getData(e){
     const {target} = dataset;
     const apartmentData = data.filter(d => d.category === target);
     const html = getMarkup(apartmentData);
+    const body = document.body;
 
     mainDiv.innerHTML = html;
     const cardClose = document.querySelector('.card__close');
